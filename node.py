c1ff46335680608297abdd321757ebcdbddc29cb
= <gdroel@gmail.com>
2019-05-21 18:54:59 -0700

    removed print statements

--- node.py
+++ node.py
@@ -26,21 +26,22 @@ class Node:
 
         # our hashed value neesd to be less than this   
         lessThan = 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+        # set the digest as the highest possible value so the loop runs at least once
         digest = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 
         print(lessThan)
         # keep generating values while it's less than this value
         while(digest > lessThan):
             # generate the nonce
-            print("checking")
             nonce = random.getrandbits(32)
             unhashed = transactionJSON + str(nonce)
             sha256 = hashlib.new("sha256")
             sha256.update(unhashed.encode('utf-8'))
+
+            # convert digest to int for comparison
             digest = int(sha256.hexdigest(), 16)
 
-        print(hex(digest))
-        print("transaction verified")
             
 
 
