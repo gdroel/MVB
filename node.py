bf9be5228e25edba7c0c526f6367e6e6a048306e
= <gdroel@gmail.com>
2019-05-21 18:48:16 -0700

    i think proof of work is working

--- node.py
+++ node.py
@@ -1,12 +1,18 @@
+import hashlib
+import uuid
+import random
+
 class Node:
     def __init__(self):
         self.prev = "hashstring"
 
     # Select an unverified transaction at random from pool
     def select_transaction(self):
+      print("select tranasction")
 
     # Check that a transaction is valid
     def validate_transaction(self):
+        print("validate")
         """
         The transaction that is being transferred is addressed to Bob.
         Bob has not previously transfered the same transaction to anyone else.
@@ -14,4 +20,36 @@ class Node:
 
     # Verify the transaction through proof-of-work
     def verify_transaction(self):
+        # for testing purposes only
+        file = open("example_transaction.json")
+        transactionJSON = file.read()
 
+        # our hashed value neesd to be less than this   
+        lessThan = 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+        digest = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+        print(lessThan)
+        # keep generating values while it's less than this value
+        while(digest > lessThan):
+            # generate the nonce
+            print("checking")
+            nonce = random.getrandbits(32)
+            unhashed = transactionJSON + str(nonce)
+            sha256 = hashlib.new("sha256")
+            sha256.update(unhashed.encode('utf-8'))
+            digest = int(sha256.hexdigest(), 16)
+
+        print(hex(digest))
+        print("transaction verified")
+            
+
+
+
+
+
+
+        
+
+        
+
+        
