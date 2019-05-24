2160d81136245ac428660fcf4a157553694e62d5
Noah Stapp <noah@noahstapp.com>
2019-05-23 18:30:31 -0700

    Transaction signatures added

--- create_transaction.py
+++ create_transaction.py
@@ -1,44 +1,68 @@
 import json
 import hashlib
+from ecdsa import SigningKey
 
 # Create a new transaction
-def create_transaction(input_set, output_set, type, signature_set):
+def create_transaction(signing_keys, transactions, input_set, output_set, type):
     test_transaction = {}
 
     number_plaintext = ""
+    number_plaintext += type
     test_transaction["TYPE"] = type
     test_transaction["INPUT"] = []
     test_transaction["OUTPUT"] = []
     test_transaction["SIGNATURE"] = []
 
-    for input_number, input_output in input_set.items():
-        test_transaction["INPUT"].append({input_number: input_output})
-        number_plaintext += input_number
-        number_plaintext += input_output
+    for input_pair in input_set:
+        if input_pair is not None:
+            test_transaction["INPUT"].append(input_pair)
+            number_plaintext += input_pair[0]
+            number_plaintext += str(input_pair[1])
+        else:
+            test_transaction["INPUT"].append(None)
     
-    for output_value, output_key in output_set.items():
-        test_transaction["OUTPUT"].append({output_value: output_key})
-        number_plaintext += output_value
-        number_plaintext += output_key
+    for output_pair in output_set:
+        print(output_pair)
+        test_transaction["OUTPUT"].append((output_pair))
+        number_plaintext += output_pair[0]
+        number_plaintext += str(output_pair[1])
 
-    for sig_id, sig in signature_set.items():
-        test_transaction["SIGNATURE"].append({sig_id: sig})
-        number_plaintext += sig_id
-        number_plaintext += sig
+    if signing_keys is not None:
+        for key in signing_keys:
+                signature = key.sign(number_plaintext.encode('utf-8')).hex()
+                test_transaction["SIGNATURE"].append(signature)
+                number_plaintext += str(signature)
+    else:
+        test_transaction["SIGNATURE"].append(None)
 
     sha3 = hashlib.new("sha256")
     sha3.update(number_plaintext.encode('utf-8'))
-
+    
     test_transaction["NUMBER"] = sha3.hexdigest()
-
-    test_transaction = json.dumps(test_transaction)
-
-    transaction_file = open("example_transaction.json", "w")
-    transaction_file.write(test_transaction)
-
+    transactions.append(test_transaction)
 
 def main():
-    create_transaction({"block1": "output1"}, {"100": "publickey1"}, "TRANS", {"type": "sig1", "input": "sig2", "output": "sig3"})
+    signing_key_1 = SigningKey.generate()
+    verifying_key_1 = signing_key_1.get_verifying_key().to_string().hex()
+
+    signing_key_2 = SigningKey.generate()
+    verifying_key_2 = signing_key_1.get_verifying_key().to_string().hex()
+
+
+    transactions = []
+    create_transaction(None, transactions, [None], [(verifying_key_1, 25)], "TRANS")
+    prev_block = transactions[0]["NUMBER"]
+    create_transaction([signing_key_1], transactions, [(prev_block, 25)], [(verifying_key_2, 25)], "TRANS")
+    # create_transaction(transactions, [("block1", 0)], [("publickey2", 100)], "JOIN")
+    # create_transaction(transactions, {"block3": "output3"}, [("publickey1", 30)], "JOIN")
+    # create_transaction(transactions, {"block4": "output4"}, [("publickey3", 40)], "MERGE")
+    # create_transaction(transactions, {"block5": "output5"}, [("publickey2", 10)], "TRANS")
+
+    transactions = json.dumps(transactions)
+
+    transaction_file = open("unverified_pool.json", "w")
+    transaction_file.write(transactions)
+
 
 if __name__ == "__main__":
     main()
--- node.py
+++ node.py
@@ -1,9 +1,20 @@
+import json
+import random
+
 class Node:
     def __init__(self):
         self.prev = "hashstring"
+        self.current_transaction = None
 
     # Select an unverified transaction at random from pool
     def select_transaction(self):
+        with open("unverified_pool.json") as file:
+            data = json.load(file)
+            random_index = random.randint(0, len(data) - 1)
+            transaction = data[random_index]
+            self.current_transaction = transaction
+            print(self.current_transaction)
+
 
     # Check that a transaction is valid
     def validate_transaction(self):
@@ -11,7 +22,16 @@ class Node:
         The transaction that is being transferred is addressed to Bob.
         Bob has not previously transfered the same transaction to anyone else.
         """
+        
 
-    # Verify the transaction through proof-of-work
-    def verify_transaction(self):
+    # # Verify the transaction through proof-of-work
+    # def verify_transaction(self):
 
+
+def main():
+    node = Node()
+    node.select_transaction()
+    node.select_transaction()
+
+if __name__ == "__main__":
+    main()
