1621ae7894b6c598545ead4d87d265ba84eaac82
Noah Stapp <noah@noahstapp.com>
2019-05-29 15:53:54 -0700

    Super broken list as pool

--- node.py
+++ node.py
@@ -7,197 +7,226 @@ import time
 from ecdsa import SigningKey, VerifyingKey, BadSignatureError
 from blockchain import Blockchain
 from block import Block
+import threading
+
+lock = threading.Lock()
 
 class Node:
-    def __init__(self):
-        self.chain = Blockchain()
-        self.current_transaction = None
-        self.used_inputs = []
-        self.initalize_chain()
+	def __init__(self, id):
+		self.chain = Blockchain()
+		self.current_transaction = None
+		self.used_inputs = []
+		self.initalize_chain()
+		self.id = id
 
-    # Adds the Genesis Block as the first block in the chain
-    def initalize_chain(self):
-        with open("transaction_file.json") as file:
-            data = json.load(file)
-            self.current_transaction = data[0]
-        self.create_block(0, 0)
+	# Adds the Genesis Block as the first block in the chain
+	def initalize_chain(self):
+		with open("transaction_file.json") as file:
+			data = json.load(file)
+			self.current_transaction = data[0]
+		self.create_block(0, 0)
 
-    def run(self, blockQueue):
-        while(True):
-            self.select_transaction()
-            # Validate the ransation
-            if self.validate_transaction():
-                # Verify Transaction via POW
-                verify_result = self.verify_transaction(blockQueue)
+	def run(self, blockQueue, transaction_pool):
+		while(True):
+			print("test")
+			if not blockQueue.empty:
+				item = blockQueue.get()
+				print(item)
+				if item == "Done":
+					print("Done")
+					self.print_chain()
+					print("node exit")
+					break
+				else:
+					blockQueue.put(item)
+			if self.select_transaction(transaction_pool):
+				print("Found")
+				# print(self.current_transaction)
+				# Validate the ransation
+				if self.validate_transaction():
+					# Verify Transaction via POW
+					verify_result = self.verify_transaction(blockQueue)
 
-                if verify_result == False:
-                    # This means that a block was received
-                    # event.param stores the block that was received
-                    block = blockQueue.get()
-                    self.handle_received_block(block)
-                else:
-                    # Broadcast the mined block
-                    self.broadcast_block(blockQueue)
-            else: # Remove an invalid transaction from the pool
-              self.remove_transaction()
+					if verify_result == False:
+						# This means that a block was received
+						# event.param stores the block that was received
+						block = blockQueue.get()
+						self.handle_received_block(block)
+					else:
+						# Broadcast the mined block
+						self.broadcast_block(blockQueue)
+				else: # Remove an invalid transaction from the pool
+					self.remove_transaction()
 
+	def handle_received_block(self, block):
+		print("block received")
+		print(block)
+		return
+		# unhashed = json.dumps(self.current_transaction) + str(nonce)
+		# sha256 = hashlib.new("sha256")
+		# sha256.update(unhashed.encode('utf-8'))
 
-    def handle_received_block(self, block):
-        print("block received")
-        print(block)
-        return
-        # unhashed = json.dumps(self.current_transaction) + str(nonce)
-        # sha256 = hashlib.new("sha256")
-        # sha256.update(unhashed.encode('utf-8'))
+	def broadcast_block(self, blockQueue):
+		print("broadcasting block")
+		for i in range(0, 1):
+			blockQueue.put(self.chain.head)   
+		self.print_chain()  
+			
+		self.remove_transaction()
+			
+		return
+		# print("broadcast!")
+		
+	# Select an unverified transaction at random from pool
+	def select_transaction(self, transaction_pool):
+		if len(transaction_pool) > 0:
+			with lock:
+				random_index = secrets.randbelow(len(transaction_pool))
+				self.current_transaction = transaction_pool[random_index]
+				return True
+		else:
+			return False
 
-    def broadcast_block(self, blockQueue):
-        print("broadcasting block")
-        for i in range(0, 9):
-            blockQueue.put(self.chain.head)     
-            
-         self.remove_transaction()
-            
-        return
-        # print("broadcast!")
-        
-    # Select an unverified transaction at random from pool
-    def select_transaction(self):
-        with open("unverified_pool.json") as file:
-            data = json.load(file)
-            random_index = secrets.randbelow(len(data))
-            self.current_transaction = data[random_index]
+		# with open("unverified_pool.json") as file:
+		# 	try:
+		# 		data = json.load(file)
+		# 		random_index = secrets.randbelow(len(data))
+		# 		self.current_transaction = data[random_index]
+		# 		return True
+		# 	except ValueError:
+		# 		return False
+			
+	# Check that a transaction is valid
+	def validate_transaction(self):
+		input_blocks = []
+		# Signature must be valid, inputs must not have been used before, coins in must equal coins out
+		if self.validate_signature() and self.validate_input() and self.validate_coin_amount():
+			# Add the transaction's inputs to the used input list
+			for input_block in self.current_transaction["INPUT"]:
+				self.used_inputs.append(input_block)
+				return True
+		else:
+			return False
+		
 
-    # Check that a transaction is valid
-    def validate_transaction(self):
-        input_blocks = []
-        # Signature must be valid, inputs must not have been used before, coins in must equal coins out
-        if self.validate_signature() and self.validate_input() and self.validate_coin_amount():
-            # Add the transaction's inputs to the used input list
-            for input_block in self.current_transaction["INPUT"]:
-             self.used_inputs.append(input_block)
-             return True
-        else:
-            return False
-        
+	# Validate that a transaction's sigature is valid
+	def validate_signature(self):
+		transaction_content = self.current_transaction["TYPE"]
 
-    # Validate that a transaction's sigature is valid
-    def validate_signature(self):
-        transaction_content = self.current_transaction["TYPE"]
+		for input_block in self.current_transaction["INPUT"]:
+			transaction_content += input_block[0]
+			transaction_content += str(input_block[1])
 
-        for input_block in self.current_transaction["INPUT"]:
-            transaction_content += input_block[0]
-            transaction_content += str(input_block[1])
+		for output_set in self.current_transaction["OUTPUT"]:
+			transaction_content += output_set[0]
+			transaction_content += str(output_set[1])
 
-        for output_set in self.current_transaction["OUTPUT"]:
-            transaction_content += output_set[0]
-            transaction_content += str(output_set[1])
+		for i in range(len(self.current_transaction["SIGNATURE"])):
+			input_block = self.current_transaction["INPUT"][i]
+			verifying_key = None
+			current_block = self.chain.head
+			while current_block is not None:
+				if input_block[0] == current_block.transaction["NUMBER"]:
+					verifying_key = VerifyingKey.from_string(bytes.fromhex(current_block.transaction["OUTPUT"][input_block[1]][0]))
+				current_block = current_block.prev
 
-        for i in range(len(self.current_transaction["SIGNATURE"])):
-            input_block = self.current_transaction["INPUT"][i]
-            verifying_key = None
-            current_block = self.chain.head
-            while current_block is not None:
-                if input_block[0] == current_block.transaction["NUMBER"]:
-                    verifying_key = VerifyingKey.from_string(bytes.fromhex(current_block.transaction["OUTPUT"][input_block[1]][0]))
-                current_block = current_block.prev
+			if verifying_key is not None:
+				signature = self.current_transaction["SIGNATURE"][i]
+				try:
+					verifying_key.verify(bytes.fromhex(signature), transaction_content.encode("utf-8"))
+					transaction_content += signature
+				except BadSignatureError:
+					print("Invalid signature")
+					return False
+			else:
+				print("Input does not exist")
+				return False
+		return True
 
-            if verifying_key is not None:
-                signature = self.current_transaction["SIGNATURE"][i]
-                try:
-                    verifying_key.verify(bytes.fromhex(signature), transaction_content.encode("utf-8"))
-                    transaction_content += signature
-                except BadSignatureError:
-                    print("Invalid signature")
-                    return False
-            else:
-                print("Input does not exist")
-                return False
-        return True
+	# Validate that a transaction's inputs have not been previously used
+	def validate_input(self):
+		for input_block in self.current_transaction["INPUT"]:
+			if input_block in self.used_inputs:
+				print("Input used previously")
+				return False
+		return True
+	
+	# Validate that the total amount of coins in a transaction's inputs equals the total amount of coins in its outputs
+	def validate_coin_amount(self):
+		input_sum = 0
+		output_sum = 0
+		for input_block in self.current_transaction["INPUT"]:
+			current_block = self.chain.head
+			while current_block is not None:
+				if input_block[0] == current_block.transaction["NUMBER"]:
+					input_sum += current_block.transaction["OUTPUT"][input_block[1]][1]
+				current_block = current_block.prev
 
-    # Validate that a transaction's inputs have not been previously used
-    def validate_input(self):
-        for input_block in self.current_transaction["INPUT"]:
-            if input_block in self.used_inputs:
-                print("Input used previously")
-                return False
-        return True
-    
-    # Validate that the total amount of coins in a transaction's inputs equals the total amount of coins in its outputs
-    def validate_coin_amount(self):
-        input_sum = 0
-        output_sum = 0
-        for input_block in self.current_transaction["INPUT"]:
-            current_block = self.chain.head
-            while current_block is not None:
-                if input_block[0] == current_block.transaction["NUMBER"]:
-                    input_sum += current_block.transaction["OUTPUT"][input_block[1]][1]
-                current_block = current_block.prev
+		for output_block in self.current_transaction["OUTPUT"]:
+			output_sum += output_block[1]
 
-        for output_block in self.current_transaction["OUTPUT"]:
-            output_sum += output_block[1]
+		if input_sum == output_sum:
+			return True
+		else:
+			print("Input does not equal output")
+			return False
 
-        if input_sum == output_sum:
-            return True
-        else:
-            print("Input does not equal output")
-            return False
+	# Verify the transaction through proof-of-work
+	# Returns True if it mined block, false otherwise
+	def verify_transaction(self, blockQueue):
 
-    # Verify the transaction through proof-of-work
-    # Returns True if it mined block, false otherwise
-    def verify_transaction(self, blockQueue):
+		# our hashed value neesd to be less than this   
+		lessThan = 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 
-        # our hashed value neesd to be less than this   
-        lessThan = 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+		# set the digest as the highest possible value so the loop runs at least once
+		digest = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 
-        # set the digest as the highest possible value so the loop runs at least once
-        digest = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+		# we keep track of the nonce
+		nonce = 0
 
-        # we keep track of the nonce
-        nonce = 0
+		# keep generating values while it's less than this value
+		while(digest > lessThan):
+			# if there's nothing in the block queue, then we keep looking 
+			if blockQueue.empty():
+				# generate the nonce
+				nonce = random.getrandbits(32)
+				unhashed = json.dumps(self.current_transaction) + str(nonce)
+				sha256 = hashlib.new("sha256")
+				sha256.update(unhashed.encode('utf-8'))
 
-        # keep generating values while it's less than this value
-        while(digest > lessThan):
-            # if there's nothing in the block queue, then we keep looking 
-            if blockQueue.empty():
-                # generate the nonce
-                nonce = random.getrandbits(32)
-                unhashed = json.dumps(self.current_transaction) + str(nonce)
-                sha256 = hashlib.new("sha256")
-                sha256.update(unhashed.encode('utf-8'))
+				# convert digest to int for comparison
+				digest = int(sha256.hexdigest(), 16)
+			else:
+				return False
 
-                # convert digest to int for comparison
-                digest = int(sha256.hexdigest(), 16)
-            else:
-                return False
+		print("block found on a thread")
+		self.create_block(nonce, digest)
 
-        print("block found on a thread")
-        self.create_block(nonce, digest)
+		return True
 
-        return True
+	# Create a new block from a verified transaction
+	def create_block(self, nonce, digest):
+		new_block = Block(nonce, digest, self.current_transaction)
+		self.chain.add_block(new_block)
 
-    # Create a new block from a verified transaction
-    def create_block(self, nonce, digest):
-        new_block = Block(nonce, digest, self.current_transaction)
-        self.chain.add_block(new_block)
-
-    # Remove a transaction from the network
-    def remove_transaction(self):
-        with open("unverified_pool.json", "r") as file:
-            data = json.load(file)
-        with open("unverified_pool.json", "w") as file:
-            data.remove(self.current_transaction)
-            new_pool = json.dumps(data)
-            file.write(new_pool)
-        
-    def print_chain(self):
-        blocks = []
-        current_block = self.chain.head
-        while current_block is not None:
-            data = {}
-            data["NONCE"] = current_block.nonce
-            data["POW"] = current_block.proof_of_work
-            data["TRANSACTION"] = current_block.transaction
-            blocks.append(data)
-            current_block = current_block.prev
-        with open("node_1_results.json", "w") as file:
-            file.write(json.dumps(blocks))
+	# Remove a transaction from the network
+	def remove_transaction(self):
+		with open("unverified_pool.json", "r") as file:
+			data = json.load(file)
+		with open("unverified_pool.json", "w") as file:
+			data.remove(self.current_transaction)
+			new_pool = json.dumps(data)
+			file.write(new_pool)
+		
+	def print_chain(self):
+		blocks = []
+		current_block = self.chain.head
+		while current_block is not None:
+			data = {}
+			data["NONCE"] = current_block.nonce
+			data["POW"] = current_block.proof_of_work
+			data["TRANSACTION"] = current_block.transaction
+			blocks.append(data)
+			current_block = current_block.prev
+		with open("node_"+str(self.id)+"_results.json", "w") as file:
+			file.write(json.dumps(blocks))
--- simulation.py
+++ simulation.py
@@ -5,51 +5,50 @@ import time
 import threading
 import queue
 
-def add_transaction(i):
-    with open("transaction_file.json", "r") as transaction_file:
-        data = json.load(transaction_file)
-        with open("unverified_pool.json", "r") as current_pool:
-            pool = json.load(current_pool)
-            with open("unverified_pool.json", "w") as unverified_pool:
-                transaction = data[i]
-                pool.append(transaction)
-                unverified_pool.write(json.dumps(pool))
-    time.sleep(secrets.randbelow(200) / 100)
+# def add_transaction(i):
+#     with open("transaction_file.json", "r") as transaction_file:
+#         data = json.load(transaction_file)
+#                 transaction = data[i]
+#                 pool.append(transaction)
+#                 unverified_pool.write(json.dumps(pool))
+#     time.sleep(secrets.randbelow(200) / 100)
 
 def main():
-    with open("transaction_file.json", "r") as transaction_file:
-        data = json.load(transaction_file)
-        with open("unverified_pool.json", "w") as unverified_pool:
-            unverified_pool.write(json.dumps([]))
+    # with open("transaction_file.json", "r") as transaction_file:
+    #     data = json.load(transaction_file)
+    #     with open("unverified_pool.json", "w") as unverified_pool:
+    #         unverified_pool.write(json.dumps([]))
         
     nodes = []
 
     # create all the nodes
-    for i in range(0, 10):
-        newNode = Node()
+    for i in range(0, 1):
+        newNode = Node(i)
         nodes.append(newNode)
 
     # create the event
     event = threading.Event()
 
     blockQueue = queue.Queue()
-
+    transaction_pool = []
     for node in nodes:
         # create all the threads 
-        threading.Thread(target = node.run, args=[blockQueue]).start()
+        threading.Thread(target = node.run, args=[blockQueue, transaction_pool]).start()
 
     i = 1
+    with open("transaction_file.json", "r") as transaction_file:
+        data = json.load(transaction_file)
     while True:
         if i < 11:
-            add_transaction(i)
-        unverified_pool = open("unverified_pool.json", "r")
-        transactions = json.load(unverified_pool)
-        if len(transactions) == 0:
+            transaction = data[i]
+            for j in range(1):
+                transaction_pool.append(transaction)
+                time.sleep(secrets.randbelow(200) / 100)
+        if len(transaction_pool) == 0:
+            blockQueue.put("Done")
+            print("Sim exit")
             break
-        else:
-            node.run()
         i += 1
-    node.print_chain()
 
 if __name__ == "__main__":
     main()
